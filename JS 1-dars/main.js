d15ca4633ee88ffb639bdd8622d15178de577edb
Islom Turgunov <147710629+ToshmirzaevNomonjon@users.noreply.github.com>
2023-10-16 18:34:02 +0500

    JS 1-dars
    27-guruh

---
+++ JS 1-dars/main.js
@@ -0,0 +1,44 @@
+/*console.log("Men console oynasida ko'rinaman!");*/
+/*document.write("Men body oynasida ko'rinaman!")*/
+/*alert("Saytimizga xush kelibsiz!")*/
+/*console.log(2+2);*/
+
+/*let x = 2;*/
+/*let y = 5;*/
+/*let z = 10*/
+/*x = 7;*/
+/*let x = 2, y = 5, z = 10;*/
+/*document.write(x+y+z);*/
+
+/*let salom = "Assalomu aleykum";*/
+/*let rang = "#f1f1f1"*/
+/*document.write("rang");*/
+
+/*let x = 5;*/
+/*let y ="JavaScript";*/
+/*document.write(typeof y);*/
+
+let x = 5;
+
+/*x+=1;*/
+/*x-=1;*/
+/*x*=1;*/
+/*x/=1;*/
+/*let qoldiq = x%4;*/
+/*let daraja = x **2;*/
+--x;
+--x;
+--x;
+--x;
+--x;
+
+document.write(x);
+
+
+
+
+
+
+
+
+
